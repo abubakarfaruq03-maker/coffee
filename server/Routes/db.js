7887d88d3b09bc567c0a189614e264cb79a386f2
Faruq Abubakar <abubakarfaruq03@gmail.com>
2026-02-19 16:00:10 +0000

    fix

--- server/Routes/db.js
+++ server/Routes/db.js
@@ -5,17 +5,14 @@ dotenv.config();
 
 const { Pool } = pg; 
 
-// We check if DATABASE_URL exists (Production). 
-// If not, it falls back to your local individual variables.
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  /* SSL is required for Neon/Vercel. 
-     We only turn it on if we are connecting to the cloud.
-  */
+//  SSl for Noen
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
-// Fallback for local development if DATABASE_URL isn't set yet
+// Fallback for local development 
 if (!process.env.DATABASE_URL) {
   pool.options.user = process.env.PG_USER;
   pool.options.host = process.env.PG_HOST;
